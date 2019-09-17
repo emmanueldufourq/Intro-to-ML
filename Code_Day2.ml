fdb75a312954450102749f4423d281d4fff2b7d6
Emmanuel Dufourq <emmanueldufourq@users.noreply.github.com>
2019-09-17 12:49:03 +0200

    Create Code_Day2.ml

---
+++ Code_Day2.ml
@@ -0,0 +1,41 @@
+# Links to the code:
+
+Each of these open up in Google Colab. To edit the code and make modifications you can do the following: 
+1. Open a link below
+2. Click on *File*
+3. Select *Save a copy in drive...* - note that this might require you to sign in with a Google account.
+
+## Random Forests Prac 1
+https://colab.research.google.com/drive/1vJwIsAKOBp6iOXmThl1ulnprLGJv_IQW
+
+## Hyper-parameter Tuning Prac 1
+https://colab.research.google.com/drive/1H-oCDpUKV1CljYGiut8tInHyOEhlThQA
+
+t-SNE Prac 1
+https://colab.research.google.com/drive/1IGfziA1ZKFNt1lQqQPmJe6Fuw80TbzrY
+
+t-SNE Prac 2
+https://colab.research.google.com/drive/1SUtmf4Qf62uQh2WCU1F1eVVECUINv69B
+
+Isolation Forest Prac 1
+https://colab.research.google.com/drive/1rqukD0toChExALJ6KhkdMgEFD85U9ZDa
+
+K-means Prac 1
+https://colab.research.google.com/drive/1oa3xfwDqygwgtZ7PoNQ02-_vC_R46CGa
+
+K-means Prac 2
+https://colab.research.google.com/drive/1Khps_JaBT80Twti7OPimdHf7Zc3K52i0
+
+<hr>
+
+## Assignment 3 - Random Forests
+https://colab.research.google.com/drive/1Gfc2dck9dmSQlaM26w3jTYuiQsy-XtsB
+
+## Assignment 4 - Hyper-parameter Tuning
+https://colab.research.google.com/drive/1dD9CEFmc-NDeZVx1wk5INzo1aWI1aVy1
+
+## Assignment 5 - K-means
+https://colab.research.google.com/drive/1JD6gxMSGIlmSohRlBWAsTpJS28aLg06U
+
+
+
